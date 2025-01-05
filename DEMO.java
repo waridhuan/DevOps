6bc3e0c555206e32bd0d13d1d4759340dbd8f9b1
waridhuan <waridhuan@gmail.com>
2025-01-05 12:57:58 +0800

    Testing files commit

---
+++ DEMO.java
@@ -0,0 +1,9 @@
+public class DEMO
+{
+	public static void main(String[] args)
+	{
+		System.out.printIn(”Welcome to Java”);
+		System.out.printIn(”Welcome to DevOps”);
+		System.out.printIn(”Git is fun”);
+	}
+}
